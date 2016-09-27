97397ac766e8840a384801e7027d3eba332d11b4
CC1993 <153718331@qq.com>
2016-09-27 19:36:47 +0800

    9.27 pushing

--- app/src/main/java/com/example/houzhe/weatheronclass/MainActivity.java
+++ app/src/main/java/com/example/houzhe/weatheronclass/MainActivity.java
@@ -2,16 +2,26 @@ package com.example.houzhe.weatheronclass;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.houzhe.weatheronclass.util.NetUtil;
 
 /**
  * Created by houzhe on 16/9/21.
  */
-
 public class MainActivity extends Activity {
 
     @Override
-    protected  void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);//加载布局
+
+        if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+            Log.d("myWeather", "网络ok");
+            Toast.makeText(MainActivity.this, "网络ok!", Toast.LENGTH_LONG).show();
+        }else{
+            Log.d("myWeather", "网络不ok");
+            Toast.makeText(MainActivity.this, "网络不ok!", Toast.LENGTH_LONG).show();
+        }
     }
 }
